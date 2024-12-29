d591ca950c366255db1534760730b15a84f081c9
jimmminni <152369058+jimmminni@users.noreply.github.com>
2024-12-29 17:09:07 +0900

    Change in master branch

--- KJM_GIT_TEST/src/mygit/MyClass.java
+++ KJM_GIT_TEST/src/mygit/MyClass.java
@@ -4,6 +4,6 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("HELLO GIT!");
 		System.out.println("This is the 2nd version");
-
+		System.out.println("My changed in master branch");
 	}
 }
