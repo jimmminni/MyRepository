86bec49880f62cf6f1864d1b9ed940f0f8202ca1
jimmminni <152369058+jimmminni@users.noreply.github.com>
2024-12-29 16:40:29 +0900

    HELLO

--- MyGitTest/src/mygit/MyClass.java
+++ MyGitTest/src/mygit/MyClass.java
@@ -1,23 +1,23 @@
-<<<<<<< HEAD
+//<<<<<<< HEAD
 package mygit;
 
 public class MyClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git!");
+		System.out.println("HELLO GIT!!");
 	}
 
 }
-=======
-package mygit;
-
-public class MyClass {
-	public static void main(String[] args) {
-		System.out.println("Hello Git!");
-		System.out.println("This is the 2nd version!");
-
-		YourClass your = new YourClass();
-		your.sayHello("Everyone");
-	}
-}
->>>>>>> refs/remotes/origin/master
+//=======
+//package mygit;
+//
+//public class MyClass {
+//	public static void main(String[] args) {
+//		System.out.println("Hello Git!");
+//		System.out.println("This is the 2nd version!");
+//
+//		YourClass your = new YourClass();
+//		your.sayHello("Everyone");
+//	}
+//}
+//>>>>>>> refs/remotes/origin/master
